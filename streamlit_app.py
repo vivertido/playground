197d4b981df9b643503f3a42a00d9f2538d054e9
Francisco Nieto <152757706+vivertido@users.noreply.github.com>
2025-04-23 20:03:04 +0000

    removed extra spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 
 import streamlit as st
 
- 
 pages ={
-    "Pages":
+    "My Stuff":
  [ 
     st.Page("pages/apis.py", title="API explorer"),
     st.Page("pages/movies.py", title="TMDB page"),
     st.Page("pages/playground.py", title="My Playground")
-
  ]
 }
-
 pg = st.navigation(pages)
 pg.run()
