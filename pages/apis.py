605c566e8901768795a6839b7ac056791d22e06f
Francisco Nieto <152757706+vivertido@users.noreply.github.com>
2025-04-23 21:28:13 +0000

    added fetch api featrure

--- pages/apis.py
+++ pages/apis.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import requests
 
 st.title("API Page")
+
+url = st.text_input("Paste URL")
+
+if st.button("Fetch Data"):
+    try:
+        response = requests.get(url).json()
+        
+        st.json(response, expanded=True)  # first 1000 characters, in case it's long
+
+    except Exception as e:
+        st.write(f"❌ Error: {e}")
