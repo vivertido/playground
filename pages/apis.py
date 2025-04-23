07fe31de35b1a1d05556a488ae387781477fe7ca
Francisco Nieto <152757706+vivertido@users.noreply.github.com>
2025-04-23 23:05:01 +0000

    added headers

--- pages/apis.py
+++ pages/apis.py
@@ -2,17 +2,29 @@ import streamlit as st
 import requests
 import pandas as pd
 
-st.title("API Page")
+st.title("Campus Admission data")
 
-url = st.text_input("Paste URL with campus and API key")
+#url = st.text_input("Paste URL with campus and API key")
+base_url = 'https://special-invention-v66rgrj4xvr5hxg7r-5000.app.github.dev'
+campus_search_endpoint = '/api/v1/campus/'
+
+api_key = st.text_input("Your API key")
+
+campus = st.selectbox(
+    "Choose campus",
+    ("Berkeley", "Davis", "Los Angeles", "Merced", "Riverside",  "San Diego", "Santa Barbara", "Santa Cruz"),
+)
 
 if st.button("Fetch Campus Data"):
     try:
-        data = requests.get(url).json()
+
+        chosen_campus = campus.lower()
+        fetch_url = base_url+campus_search_endpoint+chosen_campus+"?key="+api_key
+        data = requests.get(fetch_url).json()
         
        # st.json(data, expanded=True)   
 
-        tab1, tab2, tab3 = st.tabs(["Overview", "Demobraphics", "Feeder Schools"])
+        tab1, tab2, tab3 = st.tabs(["Overview", "Demographics", "Feeder Schools"])
  
 
         admissions = data["admissions"]
@@ -29,6 +41,7 @@ if st.button("Fetch Campus Data"):
 
         with tab2:
 
+            st.header(f"UC {data['university'].capitalize()} Demographics")
             # Demographics
             demo_df = pd.DataFrame(demographics).T
             st.subheader("Demographics Breakdown")
@@ -37,7 +50,9 @@ if st.button("Fetch Campus Data"):
             st.bar_chart(demo_df[["applied", "admitted", "enrolled"]])
 
         with tab3:
+
             # Feeder schools
+            st.header(f"UC {data['university'].capitalize()} Feeder Schools")
             schools_df = pd.DataFrame(top_feeder_schools)
             st.subheader("Top Feeder Schools")
             st.dataframe(schools_df)
